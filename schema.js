2636afc57bb161d59ef687aff455d48f615481d7
Krzysztof Hamerszmit <kkhamer@gmail.com>
2018-04-15 21:56:17 +0200

    add books types

--- schema.js
+++ schema.js
@@ -2,18 +2,39 @@
 const fetch = require('node-fetch')
 const util = require('util')
 const parseXML = util.promisify(require('xml2js').parseString)
-const { GraphQLObjectType, GraphQLSchema, GraphQLInt, GraphQLString } = require('graphql');
+const { GraphQLObjectType, GraphQLSchema, GraphQLInt, GraphQLString, GraphQLList } = require('graphql');
 
-fetch('https://www.goodreads.com/author/show.xml?id=4432&key=1n6meZqEqC8bMWgT8puaOA')
-  .then(res => res.text())
-  .then(parseXML)
+const BookType = new GraphQLObjectType({
+  name: 'Book',
+  description: '...',
+
+  fields: () => ({
+    title: {
+      type: GraphQLString,
+      resolve: xml => // can return a promise!
+        xml.title[0]
+    },
+    isbn: {
+      type: GraphQLString,
+      resolve: xml =>
+        xml.isbn[0]
+    }
+  })
+})
 
 const AuthorType = new GraphQLObjectType({
   name: 'Author',
   description: '...',
   fields: () => ({
     name: {
-      type: GraphQLString
+      type: GraphQLString,
+      resolve: xml => 
+        xml.GoodreadsResponse.author[0].name[0]
+    },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve: xml => 
+        xml.GoodreadsResponse.author[0].books[0].book
     }
   })
 })  
@@ -27,7 +48,12 @@ module.exports = new GraphQLSchema({
         type: AuthorType,
         args: {
           id: { type: GraphQLInt}
-        }
+        },
+        resolve: (root, args) => fetch(
+          `https://www.goodreads.com/author/show.xml?id=${args.id}&key=1n6meZqEqC8bMWgT8puaOA`
+        )
+          .then(res => res.text())
+          .then(parseXML)
       }
     })
   })
